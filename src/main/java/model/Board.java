193b636ac5f5519e1ae8953278856a9eabb6d547
M-oritz <104521093+M-oritz@users.noreply.github.com>
2022-06-11 00:55:53 +0200

    Implemented Board class representing the game state

---
+++ src/main/java/model/Board.java
@@ -0,0 +1,130 @@
+package model;
+
+import java.util.Optional;
+
+/**
+ * Class representing the game state.
+ * <br><br>
+ * It stores the position of the pieces, the players' castling ability, whose turn it is, if a player is in check
+ * and if a pawn may be captured en passant
+ * (= all of the information denoted by a fen string plus whether a player is in check).
+ */
+public class Board {
+
+    /**
+     * Like {@link #Board(int, Optional[][], boolean, boolean, boolean, boolean, boolean, boolean, boolean, int, int)}
+     * but without requiring a value to be set.
+     * The value may be set using Board.setPointValue() at a later time.
+     */
+    public Board(Optional<Piece>[][] spaces, boolean whiteNextMove, boolean whiteInCheck, boolean blackInCheck,
+            boolean whiteCastlingKingside, boolean whiteCastlingQueenside, boolean blackCastlingKingside,
+            boolean blackCastlingQueenside, int enPassantTargetRank, int enPassantTargetFile) {
+        this.spaces = spaces;
+        this.whiteNextMove = whiteNextMove;
+        this.whiteInCheck = whiteInCheck;
+        this.blackInCheck = blackInCheck;
+        this.whiteCastlingKingside = whiteCastlingKingside;
+        this.whiteCastlingQueenside = whiteCastlingQueenside;
+        this.blackCastlingKingside = blackCastlingKingside;
+        this.blackCastlingQueenside = blackCastlingQueenside;
+        this.enPassantTargetRank = enPassantTargetRank;
+        this.enPassantTargetFile = enPassantTargetFile;
+    }
+
+    /**
+     * Creates a new board with the specified values.
+     * @param pointValue the boards evaluated value
+     * @param spaces two dimensional array holding the chess pieces, represents the chess position
+     * @param whiteNextMove whether the enxt turn is white's
+     * @param whiteInCheck whether white is in checks
+     * @param blackInCheck whether black is in check
+     * @param whiteCastlingKingside whether white can castle kingside
+     * @param whiteCastlingQueenside whether white can castle queenside
+     * @param blackCastlingKingside whether black can castle kingside
+     * @param blackCastlingQueenside whether black can castle queenside
+     * @param enPassantTargetRank if a pawn performed a double-step in the last turn, the rank of the traversed space; -1 otherwise
+     * @param enPassantTargetFile if a pawn performed a double-step in the last turn, the file of the traversed space; -1 otherwise
+     */
+    public Board(int pointValue, Optional<Piece>[][] spaces, boolean whiteNextMove, boolean whiteInCheck, boolean blackInCheck,
+            boolean whiteCastlingKingside, boolean whiteCastlingQueenside, boolean blackCastlingKingside,
+            boolean blackCastlingQueenside, int enPassantTargetRank, int enPassantTargetFile) {
+        this(spaces, whiteNextMove, whiteInCheck, blackInCheck, whiteCastlingKingside, whiteCastlingQueenside,
+                blackCastlingKingside, blackCastlingQueenside, enPassantTargetRank, enPassantTargetFile);
+        this.pointValue = pointValue;
+    }
+
+    private Optional<Piece>[][] spaces;
+    private int pointValue;
+    private boolean whiteNextMove;
+    private boolean whiteInCheck;
+    private boolean blackInCheck;
+    private boolean whiteCastlingKingside;
+    private boolean whiteCastlingQueenside;
+    private boolean blackCastlingKingside;
+    private boolean blackCastlingQueenside;
+    private int enPassantTargetRank;
+    private int enPassantTargetFile;
+
+    /**
+     * Sets the point value of this board.
+     * Required to allow for boards to be generated first and evaluated at a later time.
+     * @param pointValue the board's value
+     */
+    public void setPointValue(int pointValue) {
+        this.pointValue = pointValue;
+    }
+
+    /*
+     **********************************
+     * Getters
+     **********************************
+     */
+    public int getPointValue() {
+        return pointValue;
+    };
+
+    public Optional<Piece>[][] getSpaces() {
+        return spaces;
+    }
+
+    public boolean getWhitesTurn() {
+        return whiteNextMove;
+    }
+
+    public boolean getWhiteInCheck() {
+        return whiteInCheck;
+    }
+
+    public boolean getBlackInCheck() {
+        return blackInCheck;
+    }
+
+    public boolean getWhiteNextMove() {
+        return whiteNextMove;
+    }
+
+    private boolean getWhiteCastlingKingside() {
+        return whiteCastlingKingside;
+    }
+
+    public boolean getWhiteCastlingQueenside() {
+        return whiteCastlingQueenside;
+    }
+
+    public boolean getBlackCastlingKingside() {
+        return blackCastlingKingside;
+    }
+
+    public boolean getBlackCastlingQueenside() {
+        return blackCastlingQueenside;
+    }
+
+    public int getEnPassantTargetRank() {
+        return enPassantTargetRank;
+    }
+
+    public int getEnPassantTargetFile() {
+        return enPassantTargetFile;
+    }
+
+}
