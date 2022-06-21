1e21a2a38b8cf3e348eccf6750e430c191f8949a
Johannes Lux <s8joluxx@stud.uni-saarland.de>
2022-06-21 18:27:31 +0200

    Implemented simple PositionEvaluator with potential for bonusses (bishop pair)

---
+++ src/main/java/positionevaluator/PositionEvaluator.java
@@ -0,0 +1,41 @@
+package positionevaluator;
+
+import model.Board;
+import model.Piece;
+import model.PieceType;
+
+public abstract class PositionEvaluator {
+    public static int evaluatePosition(Board boardState){
+        int result = 0;
+        int blackBishops = 0;
+        int whiteBishops = 0;
+        for (int rank = 0; rank < 8; rank++){
+            for (int file = 0; file < 8; file++){
+                Piece currentPiece = boardState.getPieceAt(rank, file);
+                if (currentPiece == null){
+                    continue;
+                }
+                int sign = 0;
+                if (currentPiece.getIsWhite()){
+                    if (currentPiece.getPieceType() == PieceType.BISHOP){
+                        whiteBishops++;
+                    }
+                    sign = 1;
+                } else {
+                    if (currentPiece.getPieceType() == PieceType.BISHOP){
+                        blackBishops++;
+                    }
+                    sign = -1;
+                }
+                result += sign*currentPiece.getPieceType().getPointValue();
+            }
+        }
+        if (whiteBishops >= 2){
+            result += 2;
+        }
+        if (blackBishops >= 2){
+            result -= 2;
+        }
+        return result;
+    }
+}
