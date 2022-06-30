dc62502df4ab259bb1891720d504b5466a46ab06
M-oritz <104521093+M-oritz@users.noreply.github.com>
2022-06-30 11:46:44 +0200

    tests for equals are done

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,5 +1,4 @@
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
@@ -14,38 +13,42 @@ import model.Piece;
 public class BoardTest {
 
     /**
-     * Board with every boolean set to false and every numberic value set to the lowest possible value.
+     * Board with every boolean set to false and every numberic value set to the
+     * lowest possible value.
      */
     private Board testBoard;
 
-    //TODO make testBoard its own class: extend Board, hardwire values;
-    //create 2 instances, manipulate one of them, compare
-    //implement workaround for private fields that have no getter
-    //method to create a new board with a changed value in that attribute
-
     @BeforeEach
     /**
-     * Sets up the test board with every boolean set to false and every numberic value set to the lowest possible value.
-     * Do not make changes to the Board constructor invocation, it is mirrored in some test methods.
+     * Sets up the test board with every boolean set to false and every numberic
+     * value set to the lowest possible value.
+     * Do not make changes to the Board constructor invocation, it is mirrored in
+     * some test methods.
      */
     public void setUpBoard() {
-        this.testBoard =   new Board(0, false, false, testSpaces, false, false, false, false, false, 0, 0, 0, 1);
+        this.testBoard = new Board(0, false, false, testSpaces, false, false, false, false, false, 0, 0, 0, 1);
     }
-    
+
     /**
      * take care not to make changes to this array as it is shared between tests
      */
-    private Piece[][] testSpaces =  {
-            {new Piece('p'),new Piece('B'),new Piece('r'),new Piece('n'),new Piece('Q'),new Piece('k'),new Piece('n'),new Piece('q')},
-            {new Piece('Q'),new Piece('b'),new Piece('P'),new Piece('p'),new Piece('B'),new Piece('P'),new Piece('r'),new Piece('R')},
-            {new Piece('b'),new Piece('B'),new Piece('r'),new Piece('R'),new Piece('n'),new Piece('N'),new Piece('p'),new Piece('P')},
-            {new Piece('p'),new Piece('p'),new Piece('b'),new Piece('b'),null,new Piece('r'),new Piece('n'),new Piece('P')},
-            {null, new Piece('q'),new Piece('K'),new Piece('b'),null,new Piece('P'),new Piece('N'),new Piece('p')},
-            {new Piece('n'), null,null,new Piece('N'),null,new Piece('b'),new Piece('q'),null},
-            {new Piece('r'), new Piece('N'),new Piece('P'),new Piece('P'),null,new Piece('R'),new Piece('r'),new Piece('Q')},
-            {new Piece('N'), new Piece('R'),null,new Piece('b'),null,new Piece('p'),new Piece('r'),null},
-            };
-    
+    private Piece[][] testSpaces = {
+            { new Piece('p'), new Piece('B'), new Piece('r'), new Piece('n'), new Piece('Q'), new Piece('k'),
+                    new Piece('n'), new Piece('q') },
+            { new Piece('Q'), new Piece('b'), new Piece('P'), new Piece('p'), new Piece('B'), new Piece('P'),
+                    new Piece('r'), new Piece('R') },
+            { new Piece('b'), new Piece('B'), new Piece('r'), new Piece('R'), new Piece('n'), new Piece('N'),
+                    new Piece('p'), new Piece('P') },
+            { new Piece('p'), new Piece('p'), new Piece('b'), new Piece('b'), null, new Piece('r'), new Piece('n'),
+                    new Piece('P') },
+            { null, new Piece('q'), new Piece('K'), new Piece('b'), null, new Piece('P'), new Piece('N'),
+                    new Piece('p') },
+            { new Piece('n'), null, null, new Piece('N'), null, new Piece('b'), new Piece('q'), null },
+            { new Piece('r'), new Piece('N'), new Piece('P'), new Piece('P'), null, new Piece('R'), new Piece('r'),
+                    new Piece('Q') },
+            { new Piece('N'), new Piece('R'), null, new Piece('b'), null, new Piece('p'), new Piece('r'), null },
+    };
+
     private Board generateBoardWithModififedSpaces(int rank, int file, char pieceCharacter) {
         Piece[][] modifiedSpaces = testBoard.copySpaces();
         modifiedSpaces[rank][file] = new Piece(pieceCharacter);
@@ -56,11 +59,11 @@ public class BoardTest {
     public void copySpacesTest() {
         Board board = new Board(false, false, testSpaces, false, false, false, false, false, 0, 0, 0, 1);
         Piece[][] copy = board.copySpaces();
-        //verify that a new array was created
-        //pieces need not be new instances
+        // verify that a new array was created
+        // pieces need not be new instances
         assertTrue(testSpaces != copy);
         for (int i = 0; i < 8; i++) {
-            assertTrue(testSpaces[i] != copy[i] );
+            assertTrue(testSpaces[i] != copy[i]);
             for (int j = 0; j < 8; j++) {
                 assertEquals(testSpaces[i][j], copy[i][j]);
             }
@@ -68,11 +71,17 @@ public class BoardTest {
     }
 
     @Test
-    public void cloneReturnsEqualBoardTest() {
-        Board clonedBoard = testBoard.clone();
-        assertEquals(testBoard,clonedBoard);
+    public void equalsToSelf() {
+        assertEquals(testBoard, testBoard);
     }
 
+    @Test
+    public void cloneReturnsEqualBoardTest() {
+        Board clonedBoard = testBoard.clone();
+        //cloned board needs to be a new instance
+        assertTrue(clonedBoard != testBoard);
+        assertEquals(testBoard, clonedBoard);
+    }
 
     @Test
     public void notEqualsPointValueBoardTest() {
@@ -81,7 +90,6 @@ public class BoardTest {
         assertFalse(testBoard.equals(clonedBoard));
     }
 
-    
     @Test
     public void notEqualsWhiteInCheckBoardTest() {
         Board clonedBoard = testBoard.clone();
@@ -98,44 +106,88 @@ public class BoardTest {
 
     @Test
     public void notEqualsSpacesTest() {
-        Board comparedBoard = generateBoardWithModififedSpaces(4,7,'b');
-        assertFalse(testBoard.equals(comparedBoard));        
+        Board comparedBoard = generateBoardWithModififedSpaces(4, 7, 'b');
+        assertFalse(testBoard.equals(comparedBoard));
     }
 
     @Test
     public void notEqualsA8SpaceTest() {
-        Board comparedBoard = generateBoardWithModififedSpaces(0,0,'b');
-        assertFalse(testBoard.equals(comparedBoard));        
+        Board comparedBoard = generateBoardWithModififedSpaces(0, 0, 'b');
+        assertFalse(testBoard.equals(comparedBoard));
     }
 
     @Test
     public void notEqualsH1SpaceTest() {
-        Board comparedBoard = generateBoardWithModififedSpaces(7,7,'N');
-        assertFalse(testBoard.equals(comparedBoard));        
+        Board comparedBoard = generateBoardWithModififedSpaces(7, 7, 'N');
+        assertFalse(testBoard.equals(comparedBoard));
     }
 
     @Test
     public void notEqualsA5SpaceTest() {
-        Board comparedBoard = generateBoardWithModififedSpaces(0,3,'N');
-        assertFalse(testBoard.equals(comparedBoard));        
+        Board comparedBoard = generateBoardWithModififedSpaces(0, 3, 'N');
+        assertFalse(testBoard.equals(comparedBoard));
     }
-    
+
     @Test
     public void notEqualswhiteNextMove() {
-        Board clonedBoard = testBoard.clone();
-        clonedBoard.setWhite(true);
-        assertFalse(testBoard.equals(clonedBoard));
+        Board comparedBoard = new Board(0, false, false, testBoard.copySpaces(), true, false, false, false, false, 0, 0,
+                0, 1);
+        assertFalse(testBoard.equals(comparedBoard));
     }
 
+    @Test
+    public void notEqualsWhiteCastlingKingside() {
+        Board comparedBoard = new Board(0, false, false, testBoard.copySpaces(), false, true, false, false, false, 0, 0,
+                0, 1);
+        assertFalse(testBoard.equals(comparedBoard));
+    }
 
-/*     boolean whiteNextMove, 
-    boolean whiteCastlingKingside, 
-    boolean whiteCastlingQueenside, 
-    boolean blackCastlingKingside, 
-    boolean blackCastlingQueenside, 
-    int enPassantTargetRank, 
-    int enPassantTargetFile, 
-    int halfMoves, 
-    int fullMoves
- */
+    @Test
+    public void notEqualsWhiteCastlingQueenside() {
+        Board comparedBoard = new Board(0, false, false, testBoard.copySpaces(), false, false, true, false, false, 0, 0,
+                0, 1);
+        assertFalse(testBoard.equals(comparedBoard));
+    }
+
+    @Test
+    public void notEqualsBlackCastlingKingside() {
+        Board comparedBoard = new Board(0, false, false, testBoard.copySpaces(), false, false, false, true, false, 0, 0,
+                0, 1);
+        assertFalse(testBoard.equals(comparedBoard));
+    }
+
+    @Test
+    public void notEqualsBlackCastlingQueenside() {
+        Board comparedBoard = new Board(0, false, false, testBoard.copySpaces(), false, false, false, false, true, 0, 0,
+                0, 1);
+        assertFalse(testBoard.equals(comparedBoard));
+    }
+
+    @Test
+    public void notEqualsEnPassantTargetRank() {
+        Board comparedBoard = new Board(0, false, false, testBoard.copySpaces(), false, false, false, false, false, 1,
+                0, 0, 1);
+        assertFalse(testBoard.equals(comparedBoard));
+    }
+
+    @Test
+    public void notEqualsEnPassantTargetFile() {
+        Board comparedBoard = new Board(0, false, false, testBoard.copySpaces(), false, false, false, false, false, 0,
+                1, 0, 1);
+        assertFalse(testBoard.equals(comparedBoard));
+    }
+
+    @Test
+    public void notEqualsHalfMoves() {
+        Board comparedBoard = new Board(0, false, false, testBoard.copySpaces(), false, false, false, false, false, 0,
+                0, 1, 1);
+        assertFalse(testBoard.equals(comparedBoard));
+    }
+
+    @Test
+    public void notEqualsFullMoves() {
+        Board comparedBoard = new Board(0, false, false, testBoard.copySpaces(), false, false, false, false, false, 0,
+                0, 0, 2);
+        assertFalse(testBoard.equals(comparedBoard));
+    }
 }
