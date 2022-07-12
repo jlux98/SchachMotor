c78b1a43539420cb349f01f2b68314a7a77407d9
Johannes Lux <s8joluxx@stud.uni-saarland.de>
2022-07-12 11:20:15 +0200

    Added elements for the Tokenizer

---
+++ src/main/java/uciservice/CommandType.java
@@ -0,0 +1,27 @@
+package uciservice;
+
+public enum CommandType {
+    BINC,
+    BTIME,
+    DEPTH,
+    GO,
+    INFINITE,
+    ISREADY,
+    MATE,
+    MOVES,
+    MOVESTOGO,
+    MOVETIME,
+    NAME,
+    NODES,
+    PONDER,
+    PONDERHIT,
+    POSITION,
+    QUIT
+    SETOPTION,
+    STOP,
+    UCI,
+    UCINEWGAME,
+    VALUE,
+    WINC,
+    WTIME,
+}
