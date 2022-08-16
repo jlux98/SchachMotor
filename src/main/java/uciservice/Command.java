42cf94a80c7d475d2e4399cfb2184a05f18ccced
Johannes Lux <s8joluxx@stud.uni-saarland.de>
2022-08-16 19:56:00 +0200

    Writing the first draft of the tokenizer

--- src/main/java/uciservice/Command.java
+++ src/main/java/uciservice/Command.java
@@ -17,4 +17,26 @@ public class Command {
         this.data = data;
     }
     
+
+    public Command(CommandType type, String data) {
+        this.type = type;
+        this.data = data;
+    }
+
+    public Command(CommandType type) {
+        this.type = type;
+        this.data = "";
+    }
+    
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Command)) {
+            return false;
+        }
+        Command command = (Command) o;
+        return type == command.type && data.equals(command.data);
+    }
 }
