2c2c8f6dcac209a6d73934719f0374be66bb227b
Johannes Lux <s8joluxx@stud.uni-saarland.de>
2022-09-02 22:34:13 +0200

    connection to GUI established

---
+++ src/main/java/uciservice/UCIParser.java
@@ -0,0 +1,87 @@
+package uciservice;
+
+import java.util.List;
+
+public abstract class UCIParser{
+    public static void executeCommand(Command command){
+        switch(command.getType()){
+            case BINC:
+                break;
+            case BTIME:
+                break;
+            case CODE:
+                break;
+            case CONSTANT:
+                break;
+            case DEBUG:
+                break;
+            case DEPTH:
+                break;
+            case FEN:
+                break;
+            case GO:
+                /*
+                 * Supported arguments:
+                 * - searchmoves
+                 * - ponder (if there is time to implement this)
+                 * - depth
+                 * - nodes?
+                 * - movetime
+                 * - infinite
+                 */
+                // List<Command> children = command.getChildren();                break;
+            case INFINITE:
+                break;
+            case ISREADY:
+                System.out.println("readyok");
+                break;
+            case LATER:
+                break;
+            case MATE:
+                break;
+            case MOVES:
+                break;
+            case MOVESTOGO:
+                break;
+            case MOVETIME:
+                break;
+            case NAME:
+                break;
+            case NODES:
+                break;
+            case PONDER:
+                break;
+            case PONDERHIT:
+                break;
+            case POSITION:
+                break;
+            case QUIT:
+                break;
+            case REGISTER:
+                break;
+            case SEARCHMOVES:
+                break;
+            case SETOPTION:
+                break;
+            case STARTPOS:
+                break;
+            case STOP:
+                break;
+            case UCI:
+                System.out.println("uciok");
+                
+                break;
+            case UCINEWGAME:
+                break;
+            case VALUE:
+                break;
+            case WINC:
+                break;
+            case WTIME:
+                break;
+            default:
+                break;
+
+        }
+    }
+}
