a4702b2536942eef8801a8dbd0afc2a168f21ecc
M-oritz <104521093+M-oritz@users.noreply.github.com>
2022-09-09 15:19:15 +0200

    reduced test depth to 3 - reduced depth of alpha-beta used in GameTreePackageTest from 5 to 3 to speed up the tests

--- src/test/java/tests/GameTreePackageTest.java
+++ src/test/java/tests/GameTreePackageTest.java
@@ -54,8 +54,6 @@ public class GameTreePackageTest {
         Position pos1 = FenParser.parseFen(fen);
         GameNodeAlphaBetaPruning pruning1 = new GameNodeAlphaBetaPruning();
         Move result1 = new ImpGameTree(pos1, pruning1).calculateBestMove(depth).getContent().getMove();
-        // FIXME storing result1 stores a big part of the tree = a lot of memory,
-        // does behavior change if only the move is stored?
         Position pos2 = FenParser.parseFen(fen);
         GameNodeAlphaBetaPruning pruning2 = new GameNodeAlphaBetaPruning();
         Move result2 = new ImpGameTree(pos2, pruning2).calculateBestMove(depth).getContent().getMove();
@@ -89,37 +87,37 @@ public class GameTreePackageTest {
     @Test
     public void evaluatorEvaluateTreeConsistentTest1() {
         String fen = "k7/3r4/1p1b4/4n3/1R6/8/2N1P3/K7 b - - 0 1";
-        testEvaluatorEvaluateTreeConsistency(fen, 5, false);
+        testEvaluatorEvaluateTreeConsistency(fen, 3, false);
     }
 
     @Test
     public void evaluatorEvaluateTreeConsistentTest2() {
         String fen = "2K5/8/8/8/8/3RRR2/n2RRR2/2k5 w - - 0 1";
-        testEvaluatorEvaluateTreeConsistency(fen, 5, true);
+        testEvaluatorEvaluateTreeConsistency(fen, 3, true);
     }
 
     @Test
     public void treeCalculateBestMoveConsistentTest1() {
         String fen = "1r1b4/6k1/5n2/6p1/3N4/4B3/3R4/K7 w - - 0 1";
-        testTreeCalculateBestMoveConsistency(fen, 5, true);
+        testTreeCalculateBestMoveConsistency(fen, 3, true);
     }
 
     @Test
     public void treeCalculateBestMoveConsistentTest2() {
         String fen = "k7/6r1/8/3n1P2/8/1P3N1r/3NB3/K7 b - - 0 1";
-        testTreeCalculateBestMoveConsistency(fen, 5, false);
+        testTreeCalculateBestMoveConsistency(fen, 3, false);
     }
 
     @Test
     public void evaluatorEvaluateTreeEqualsTreeCalculateBestMoveTest1() {
         String fen = "2K5/8/8/8/8/3RRR2/n2RRR2/2k5 w - - 0 1";
-        testEvaluatorEvaluateTreeEqualsTreeCalculateBestMove(fen, 5, true);
+        testEvaluatorEvaluateTreeEqualsTreeCalculateBestMove(fen, 3, true);
     }
 
     @Test
     public void evaluatorEvaluateTreeEqualsTreeCalculateBestMoveTest3() {
         String fen = "k7/2n5/3b1b2/3p4/8/2N3P1/4R3/K7 b - - 0 1";
-        testEvaluatorEvaluateTreeEqualsTreeCalculateBestMove(fen, 5, false);
+        testEvaluatorEvaluateTreeEqualsTreeCalculateBestMove(fen, 3, false);
     }
 
     /**
@@ -128,25 +126,25 @@ public class GameTreePackageTest {
     @Test
     public void evaluatorEvaluateTreeEqualsTreeCalculateBestMoveTestApparentlyWorkingFen() {
         String fen = "k7/8/6p1/3rR3/2n5/1P2Bb2/8/K7 b - - 0 1";
-        testEvaluatorEvaluateTreeEqualsTreeCalculateBestMove(fen, 5, false);
+        testEvaluatorEvaluateTreeEqualsTreeCalculateBestMove(fen, 3, false);
     }
 
     @Test
     public void evaluatorEvaluateTreeConsistencyTestApparentlyWorkingFen() {
         String fen = "k7/8/6p1/3rR3/2n5/1P2Bb2/8/K7 b - - 0 1";
-        testEvaluatorEvaluateTreeConsistency(fen, 5, false);
+        testEvaluatorEvaluateTreeConsistency(fen, 3, false);
     }
 
     @Test
     public void treeCalculateBestMoveConsistencyTestApparentlyWorkingFen() {
         String fen = "k7/8/6p1/3rR3/2n5/1P2Bb2/8/K7 b - - 0 1";
-        testTreeCalculateBestMoveConsistency(fen, 5, false);
+        testTreeCalculateBestMoveConsistency(fen, 3, false);
     }
 
     @Test
     public void chaosTest() {
         String fen = "2K5/8/8/8/8/3RRR2/n2RRR2/2k5 w - - 0 1";
-        int depth = 5;
+        int depth = 3;
         Position pos = FenParser.parseFen(fen);
         GameNodeAlphaBetaPruning pruningA = new GameNodeAlphaBetaPruning();
         GameNode faulty = pruningA.evaluateTree(new ImpGameTree(pos, pruningA), depth, true);
