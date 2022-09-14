75c23e2d51f1a67080d67b9c9cad09b784ec43a1
M-oritz <104521093+M-oritz@users.noreply.github.com>
2022-09-14 19:40:48 +0200

    added gamenode variant of selfdestructing alphabeta

---
+++ src/main/java/minimax/GameNodeSelfDestructingAlphaBetaPruning.java
@@ -0,0 +1,21 @@
+package minimax;
+
+import gametree.GameNode;
+import gametree.Node;
+import gametree.Tree;
+import model.Position;
+
+public class GameNodeSelfDestructingAlphaBetaPruning extends SelfDestructingAlphaBetaPruning<Position> implements GameTreeEvaluator {
+    @Override
+    public GameNode evaluateTree(Tree<? extends Node<Position>> tree, int depth, boolean whitesTurn) {
+        return evaluateNode(tree.getRoot(), depth, whitesTurn);
+    }
+
+    /**
+     * @deprecated
+     */
+    @Override
+    public GameNode evaluateNode(Node<Position> node, int depth, boolean whitesTurn) {
+        return (GameNode) super.evaluateNode(node, depth, whitesTurn);
+    }
+}
