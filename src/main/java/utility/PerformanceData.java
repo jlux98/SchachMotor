89ce20c0f2bf523f03e22d18dec32752a3045783
M-oritz <104521093+M-oritz@users.noreply.github.com>
2022-09-30 12:38:07 +0200

    demo benchmark mode now measures attack map time

--- src/main/java/utility/PerformanceData.java
+++ src/main/java/utility/PerformanceData.java
@@ -1,7 +1,11 @@
 package utility;
 
+/**
+ * Class holding static fields used in performance evaluation.
+ */
 public class PerformanceData {
     public static long moveGenerationTime = 0;
+    public static long attackMapGenerationTime = 0;
     public static int getOrComputeStaticValueCalls = 0;
     public static int getOrComputeLeafValueCalls = 0;
     public static int staticValueComputations = 0;
